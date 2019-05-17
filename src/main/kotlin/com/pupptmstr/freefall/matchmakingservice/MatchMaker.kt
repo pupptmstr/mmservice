ad4c60e7c5797dfaa2b4b3567139818c26e8f97f
payforplayonsteam@yandex.ru <payforplayonsteam@yandex.ru>
2019-05-17 13:36:22 +0300

    Fixed bag with treeMap, replace it with treeSet.

--- src/main/kotlin/com/pupptmstr/freefall/matchmakingservice/MatchMaker.kt
+++ src/main/kotlin/com/pupptmstr/freefall/matchmakingservice/MatchMaker.kt
@@ -3,39 +3,42 @@ package com.pupptmstr.freefall.matchmakingservice
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import java.util.*
+import kotlin.Comparator
 import kotlin.math.abs
 
 class MatchMaker(channelToRead: Channel<Pair<Int, Int>>, channelToWrite: Channel<Triple<Int, Int, String>>) {
 
-    private val map = TreeMap<Int, Int>()
+    private val map = TreeSet<Pair<Int, Int>>(Comparator<Pair<Int, Int>> { p1, p2 -> p1.first - p2.first })
 
     init {
         CoroutineScope(Dispatchers.Default).launch {
-            for (msg in channelToRead){
+            for (msg in channelToRead) {
                 //msg.second = lvl. Мапа сортируется по ключам, поэтому и берем лвл
-                map[msg.second] = msg.first
+                map.add(msg.second to msg.first)
                 makeNewPairs(channelToWrite)
             }
         }
     }
 
     private suspend fun makeNewPairs(channelToWrite: Channel<Triple<Int, Int, String>>) {
-        if (map.size > 1){
+        if (map.size > 1) {
+            val list = map.toMutableList()
             for (i in 0 until map.size) {
-                val it = map.entries.iterator()
-                val playerNow = it.next()
-                val playerNext = it.next()
-                if (abs(playerNow.key - playerNext.key) < 2) {
-                    val key = generateABattleKey(playerNow.key, playerNext.key, playerNow.value, playerNext.value)
-                    map.remove(playerNow.key)
-                    map.remove(playerNext.key)
-                    channelToWrite.send(Triple(playerNow.value, playerNow.value, key))
+                val playerNow = list[i]
+                val playerNext = list[i + 1]
+                if (abs(playerNow.first - playerNext.first) < 2) {
+                    val key = generateABattleKey(playerNow.first, playerNext.first, playerNow.second, playerNext.second)
+                    map.remove(playerNow)
+                    map.remove(playerNext)
+                    list.remove(playerNow)
+                    list.remove(playerNext)
+                    channelToWrite.send(Triple(playerNow.second, playerNext.second, key))
                 }
             }
         }
     }
 
-    fun generateABattleKey(lvl1: Int, lvl2: Int, id1: Int, id2: Int) : String {
+    private fun generateABattleKey(lvl1: Int, lvl2: Int, id1: Int, id2: Int): String {
         return "battle-${lvl1.toByte()}-${lvl2.toByte()} ${id1.toByte()} ${id2.toByte()}"
     }
 }
